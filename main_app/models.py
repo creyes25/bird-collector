4b67c1333c79ad11d70ab3cd0de11b62cf690142
creyes25 <carolina.reyes2022@gmail.com>
2022-11-08 12:07:57 -0600

    create a new Feeding model and add model to admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
+FOODS = (
+  ('SE', 'Seeds'),
+  ('GR', 'Grains'),
+  ('NU', 'Nuts'),
+  ('FR', 'Fruits'),
+  ('BE', 'Berries'),
+  ('VE', 'Veggies'),
+)
+
 # Create your models here.
 class Bird(models.Model):
   name = models.CharField(max_length=100)
@@ -15,3 +30,21 @@ class Bird(models.Model):
 
   def get_absolute_url(self):
       return reverse("birds_details", kwargs={"bird_id": self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  food = models.CharField(
+    max_length=2,
+    choices=FOODS,
+    default = FOODS[0][0]
+  )
+  bird = models.ForeignKey(Bird, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_food_display()} for {self.get_meal_display()} on {self.date}"
+  
