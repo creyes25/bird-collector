eab99b7fefbd665e4de9e066492c9790f1c4c257
creyes25 <carolina.reyes2022@gmail.com>
2022-11-07 20:03:13 -0600

    add a Bird model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Bird(models.Model):
+  name = models.CharField(max_length=100)
+  type = models.CharField(max_length=100)
+  description = models.TextField(max_length=300)
+  habitat = models.CharField(max_length=100)
+  color = models.CharField(max_length=100)
+  age = models.IntegerField()
+
+  def __str__(self):
+    return self.name
+
+  # def get_absolute_url(self):
+  #     return reverse("birds_detail", kwargs={"finch_id": self.id})
