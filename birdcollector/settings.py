d6bf6d00fb88c908ddfa27781c9e9424ad327861
creyes25 <70359065+creyes25@users.noreply.github.com>
2022-12-20 23:11:52 -0600

    Update settings.py

--- birdcollector/settings.py
+++ birdcollector/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True' 
 
-ALLOWED_HOSTS = ["bird-collector-django-production.up.railway.app"]
+ALLOWED_HOSTS = ["web-production-7382.up.railway.app"]
 
 
 
